1fe7e28e79ce54782951f1d4033390c183e651d9
icode100 <ipsdas@ipsdas-PC.localdomain>
2025-12-01 07:24:56 +0000

    ed185B

---
+++ Educational Codeforces Round 185 (Rated for Div. 2)/B_Addition_on_a_Segment.py
@@ -0,0 +1,20 @@
+'''
+* the maximum possible r-l+1 can be N-nums.count(0)
+* for some i-th operation we add r-l+1 to the sum of elements 
+* so the total sum is atleast r-l+1 + (N-1) (because we can do N-1 operations adding simply 1 to each element)
+* so we need to maximize r-l+1 such that,  total >= r-l+1 + (N-1)
+* total - (N-1) >= r-l+1  and r-l+1 <= N - nums.count(0)
+* thus maximum r-l+1 such that r-l+1 <= total - (N-1) and r-l+1 <= N - nums.count(0)
+'''
+
+def function(N,arr):
+    ans,total = N-arr.count(0), sum(arr)
+    for length in range(ans,0,-1):
+        if total - (N-1) >= length:
+            return length
+
+for _ in range(int(input())):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    print(function(N,arr))
+    
